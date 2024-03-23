e3b7ca541fd7981f1defe98ede54b5b3e2c666ce
Dinesh10731j <dinesh1122@gmail.com>
2024-03-23 12:22:03 +0545

    done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom'; // Import for programmatic navigation
 
 const Signup = () => {
-  alert("Hello world")
   const [signup, setSignup] = useState({});
   const navigate = useNavigate();
 
